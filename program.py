b4b24d898d123174766e431dbcc60677b5e99c93
Yand-ai <144802052+Yand-ai@users.noreply.github.com>
2023-09-13 10:07:17 +0200

    Update program.py
    issue 1

--- program.py
+++ program.py
@@ -1,1 +1,27 @@
-chacun son code ! 
+import os 
+import openai
+import flask 
+
+# importation de la fonction load_dotenv de la bibliotheque dotenv
+from dotenv import load_dotenv
+
+#chargement des variables d'environnement du fichier .env
+load_dotenv()
+
+#recuperation et assignation des variable d'environnement
+openai.organization = os.getenv("openai_org_id", default=None)
+
+# contenant la clé pour nous connecter à l'api openai
+openai.api_key = os.getenv("openai_api_key", default=None)
+
+for k in range(3):
+    messages=[{"role": "system", "content": "Tu es un américain"}]
+    msg = input()
+    messages.append({"role": "user", "content": msg})
+    reply = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                messages=messages)
+    messages.append({"role": "assistant", "content":reply['choices'][0]['message']['content']})
+    print(reply['choices'][0]['message']['content'])
+
+
